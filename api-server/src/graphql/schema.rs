f18c24be8db178620d1ad93cd921bb855875a732
giripriyadarshan <43517605+giripriyadarshan@users.noreply.github.com>
2024-12-02 00:51:51 +0100

    products query works, but need to implement the in memory caching part if required

--- api-server/src/graphql/schema.rs
+++ api-server/src/graphql/schema.rs
@@ -6,7 +6,7 @@ use async_graphql::http::GraphiQLSource;
 use async_graphql::{Context, EmptySubscription, Object, Schema};
 use axum::response;
 use axum::response::IntoResponse;
-use sea_orm::DatabaseConnection;
+use sea_orm::{ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter};
 
 macro_rules! role_guard {
     ($($role:expr),*) => {
@@ -23,10 +23,43 @@ impl QueryRoot {
         &self,
         ctx: &Context<'_>,
         category_id: Option<i32>,
-    ) -> Result<Products, async_graphql::Error> {
+        supplier_id: Option<i32>,
+        base_product_id: Option<i32>,
+    ) -> Result<Vec<Products>, async_graphql::Error> {
+        use crate::entity::products;
         let db = ctx.data::<DatabaseConnection>()?;
         // Implement product query logic
-        unimplemented!()
+        let products = products::Entity::find()
+            .filter(match (category_id, supplier_id, base_product_id) {
+                (Some(category_id), None, None) => products::Column::CategoryId.eq(category_id),
+                (None, Some(supplier_id), None) => products::Column::SupplierId.eq(supplier_id),
+                (None, None, Some(base_product_id)) => {
+                    products::Column::BaseProductId.eq(base_product_id)
+                }
+                _ => products::Column::CategoryId
+                    .eq(category_id)
+                    .and(products::Column::SupplierId.eq(supplier_id))
+                    .and(products::Column::BaseProductId.eq(base_product_id)),
+            })
+            .all(db)
+            .await?;
+
+        let products: Vec<Products> = products
+            .into_iter()
+            .map(|product| Products {
+                product_id: product.product_id,
+                name: product.name,
+                description: product.description,
+                base_price: product.base_price.to_string(),
+                category_id: product.category_id,
+                supplier_id: product.supplier_id,
+                stock_quantity: product.stock_quantity,
+                media_paths: product.media_paths,
+                base_product_id: product.base_product_id,
+            })
+            .collect();
+
+        Ok(products)
     }
 
     #[graphql(guard = "role_guard!(ROLE_CUSTOMER)")]
