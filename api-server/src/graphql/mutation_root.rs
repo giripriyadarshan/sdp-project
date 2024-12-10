dd99d69ef5db956af90fc53d9cf69e03530a0d8d
giripriyadarshan <43517605+giripriyadarshan@users.noreply.github.com>
2024-12-10 05:06:49 +0100

    new api: register_order mutation implemented

--- api-server/src/graphql/mutation_root.rs
+++ api-server/src/graphql/mutation_root.rs
@@ -1,3 +1,4 @@
+use crate::models::orders::{Orders, RegisterOrder};
 use crate::models::products::{create_product_model, Products, RegisterProduct};
 use crate::{
     auth::auth::{Auth, RoleGuard, ROLE_CUSTOMER, ROLE_SUPPLIER},
@@ -7,8 +8,10 @@ use crate::{
     },
 };
 use async_graphql::{Context, Object};
+use sea_orm::prelude::Decimal;
 use sea_orm::{
     ActiveEnum, ActiveValue::Set, ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter,
+    TransactionTrait,
 };
 
 pub struct MutationRoot;
@@ -169,5 +172,88 @@ impl MutationRoot {
             .exec(db)
             .await?;
         Ok(update_product.into())
+    }
+
+    #[graphql(guard = "role_guard!(ROLE_CUSTOMER)")]
+    async fn register_order(
+        &self,
+        ctx: &Context<'_>,
+        input: RegisterOrder,
+        token: String,
+    ) -> Result<Orders, async_graphql::Error> {
+        use crate::entity::{order_items, orders, products};
+        let db = ctx.data::<DatabaseConnection>()?;
+        let txn = db.begin().await?;
+
+        let customer_id = Auth::verify_token(&token)?.user_id.parse::<i32>()?;
+
+        let discount_id = match &input.discount_code {
+            Some(discount_code) => {
+                let discount = products::Entity::find()
+                    .filter(products::Column::Name.eq(discount_code))
+                    .one(db)
+                    .await
+                    .map_err(|_| "Discount not found")?
+                    .map(|product| product.product_id);
+                discount
+            }
+            None => None,
+        };
+
+        let mut total_amount: f64 = 0.0;
+        for item in &input.order_items {
+            let product: products::Model = products::Entity::find_by_id(item.product_id)
+                .one(db)
+                .await
+                .map_err(|_| "Product not found")?
+                .unwrap();
+            total_amount += product.base_price.to_string().parse::<f64>()? * item.quantity as f64;
+        }
+
+        let order = orders::ActiveModel {
+            customer_id: Set(customer_id),
+            shipping_address_id: Set(input.shipping_address_id),
+            payment_method_id: Set(input.payment_method_id),
+            discount_id: Set(discount_id),
+            total_amount: Set(Decimal::from_str_exact(total_amount.to_string().as_str())?),
+            status: Set("PENDING".to_string()),
+            ..Default::default()
+        };
+
+        let insert_order = orders::Entity::insert(order)
+            .exec_with_returning(&txn)
+            .await?;
+
+        for item in &input.order_items {
+            let product: products::Model = products::Entity::find_by_id(item.product_id)
+                .one(&txn)
+                .await?
+                .unwrap();
+
+            let product_base_price = product.base_price;
+
+            let product: products::ActiveModel = products::ActiveModel {
+                stock_quantity: Set(product.stock_quantity - item.quantity),
+                ..product.into()
+            };
+
+            products::Entity::update(product)
+                .filter(products::Column::ProductId.eq(item.product_id))
+                .exec(&txn)
+                .await?;
+
+            let order_item = order_items::ActiveModel {
+                order_id: Set(insert_order.order_id),
+                product_id: Set(item.product_id),
+                quantity: Set(item.quantity),
+                unit_price: Set(product_base_price),
+                ..Default::default()
+            };
+            order_items::Entity::insert(order_item).exec(&txn).await?;
+        }
+
+        txn.commit().await?;
+
+        Ok(insert_order.into())
     }
 }
