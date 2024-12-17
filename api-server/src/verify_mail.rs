52ea6370a2ee807b7101fa2028d693e6abf08e75
giripriyadarshan <43517605+giripriyadarshan@users.noreply.github.com>
2024-12-17 15:34:09 +0100

    mail verification added

--- api-server/src/verify_mail.rs
+++ api-server/src/verify_mail.rs
@@ -12,13 +12,16 @@ pub async fn verify_mail(
     Path(token): Path<String>,
     Extension(postgres): Extension<DatabaseConnection>,
 ) -> impl IntoResponse {
-    let claims = Auth::verify_token(&token.clone())
-        .map_err(|_| AppError::Auth {
-            message: "Invalid token".to_string(),
-            code: AuthErrorCode::InvalidCredentials,
-            user_id: None,
-        })
-        .unwrap();
+    let claims = Auth::verify_token(&token.clone()).map_err(|_| AppError::Auth {
+        message: "Invalid token".to_string(),
+        code: AuthErrorCode::InvalidCredentials,
+        user_id: None,
+    });
+
+    let claims = match claims {
+        Ok(c) => c,
+        Err(e) => return e.to_string(),
+    };
 
     let user_id = claims.user_id.parse::<i32>().unwrap();
     let user = Users::find_by_id(user_id)
